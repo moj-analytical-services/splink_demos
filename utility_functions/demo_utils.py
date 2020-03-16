be8e77aacc8cc5288c76fd5b0a18e38b3400bc6f
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-03-16 14:57:41 +0000

    add save load params

--- utility_functions/demo_utils.py
+++ utility_functions/demo_utils.py
@@ -4,6 +4,35 @@ from splink.validate import _get_schema
 from IPython.display import display, Markdown
 from splink.validate import _get_schema
 
+from pyspark.context import SparkContext, SparkConf
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.types import StructType
+import pyspark.sql.functions as f
+
+def get_spark():
+    conf=SparkConf()
+
+    # Load in a jar that provides extended string comparison functions such as Jaro Winkler.
+    # Splink
+    conf.set('spark.driver.extraClassPath', 'jars/scala-udf-similarity-0.0.6.jar')
+    conf.set('spark.jars', 'jars/scala-udf-similarity-0.0.6.jar')
+
+
+    # WARNING:
+    # These config options are appropriate only if you're running Spark locally!!!
+    conf.set('spark.driver.memory', '4g')
+    conf.set("spark.sql.shuffle.partitions", "8")
+
+    sc = SparkContext.getOrCreate(conf=conf)
+
+    spark = SparkSession(sc)
+
+    # Register UDFs
+    from pyspark.sql import types
+    spark.udf.registerJavaFunction('jaro_winkler_sim', 'uk.gov.moj.dash.linkage.JaroWinklerSimilarity', types.DoubleType())
+    spark.udf.registerJavaFunction('Dmetaphone', 'uk.gov.moj.dash.linkage.DoubleMetaphone', types.StringType())
+    return spark
+
 def render_key_as_markdown(key, is_col=False):
     md = []
     schema = _get_schema()
